21aba39647b4eeba3057d50241175d8ad6b2ae09
isaacwkm <isaacwkm@gmail.com>
2024-06-17 10:02:26 -0700

    done

--- rules.js
+++ rules.js
@@ -1,23 +1,26 @@
 class Start extends Scene {
     create() {
-        this.engine.setTitle("Title goes here"); // TODO: replace this text using this.engine.storyData to find the story title
+        this.engine.setTitle(this.engine.storyData.Title); // TODO: replace this text using this.engine.storyData to find the story title
         this.engine.addChoice("Begin the story");
     }
 
     handleChoice() {
-        this.engine.gotoScene(Location, "Home"); // TODO: replace this text by the initial location of the story
+        this.engine.gotoScene(Location, this.engine.storyData.InitialLocation); // TODO: replace this text by the initial location of the story
     }
 }
 
 class Location extends Scene {
     create(key) {
-        let locationData = undefined; // TODO: use `key` to get the data object for the current story location
-        this.engine.show("Body text goes here"); // TODO: replace this text by the Body of the location data
-        
-        if(true) { // TODO: check if the location has any Choices
-            for(let choice of ["example data"]) { // TODO: loop over the location's Choices
-                this.engine.addChoice("action text"); // TODO: use the Text of the choice
-                // TODO: add a useful second argument to addChoice so that the current code of handleChoice below works
+        let locationData = this.engine.storyData.Locations[key]; // TODO: use `key` to get the data object for the current story location
+        this.engine.show(locationData.Body); // TODO: replace this text by the Body of the location data
+
+        if (locationData.Choices.length > 0) { // TODO: check if the location has any Choices
+            for (let choice of locationData.Choices) { // TODO: loop over the location's Choices
+                if(true) {
+                    this.engine.addChoice(choice.Text, choice); // TODO: use the Text of the choice
+                    // TODO: add a useful second argument to addChoice so that the current code of handleChoice below works
+
+                }
             }
         } else {
             this.engine.addChoice("The end.")
@@ -25,12 +28,40 @@ class Location extends Scene {
     }
 
     handleChoice(choice) {
-        if(choice) {
-            this.engine.show("&gt; "+choice.Text);
-            this.engine.gotoScene(Location, choice.Target);
+        if (choice) {
+            if (this.isUnlocked(choice)){
+
+                this.unlockOthers(choice);
+                this.engine.show("&gt; " + choice.Text);
+                this.engine.gotoScene(Location, choice.Target);
+            }
+            else{
+                this.engine.show("&gt; " + choice.LockedMessage);
+                this.engine.addChoice("Confirm", choice.LockedChoice);
+            }
+
         } else {
             this.engine.gotoScene(End);
         }
+    }
+
+    unlockOthers(choice){
+        if (choice.Unlocks != null){
+           let loc = this.engine.storyData.Locations[choice.Unlocks.In];
+           loc.Choices[choice.Unlocks.Target].Locked = false;
+        }
+    }
+
+    isUnlocked(choice) {
+        if (choice.Locked != null) {
+            if (choice.Locked == true) {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+        return true;
     }
 }
 
